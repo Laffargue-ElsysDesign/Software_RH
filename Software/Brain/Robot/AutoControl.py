d4a9f3519b01abe234a4e53256824239e9c642bf
Lenny Laffargue <lenny.laffargue@gmail.com>
2022-06-17 14:54:38 +0200

    Delete pyc executable

--- Software/Brain/Robot/AutoControl.py
+++ Software/Brain/Robot/AutoControl.py
@@ -25,26 +25,35 @@ class Auto_Control(Thread):
         self.Mgt.Stop = False
         self.Mgt.Waiting = False
         while True:
+            #Wait until Auto Mode gets called
             self.Wait_Start()
+
+            #Continue until AutoMode gets shut down
             while not self.Mgt.Stop:
 
                 Current_Loc = 0 #TBD
 
+                #If battery Alert, got back home asap
                 if self.Alerts.Battery:
                     if not self.Navigation.Mgt.Waiting:
                         self.Navigation.Mgt.Stop = True
                         while not self.Navigation.Mgt.Waiting:
                             pass
-                        self.Navigation.path = Dijkstra(cst.Home, Current_Loc)
+                        self.Navigation.MUT.acquire()
+                        self.Navigation.path = Dijkstra(cst.Home)
+                        self.Navigation.MUT.release()
                         self.Navigation.Mgt.Stop = False
-                        
+                
+                #If a new alert is triggeres, gets treated first
                 if self.Alerts.Balise.New:
                     if not self.Navigation.Mgt.Waiting:
                         self.Navigation.Mgt.Stop = True
                         while not self.Navigation.Mgt.Waiting:
                             pass
                     self.Alerts.Balise.MUT.acquire()
-                    self.Navigation.path = Dijkstra(self.Alerts.Balise.Loc, Current_Loc)
+                    self.Navigation.MUT.acquire()
+                    self.Navigation.path = Dijkstra(self.Alerts.Balise.Loc)
+                    self.Navigation.MUT.release()
                     self.Alerts.Balise.MUT.release()
                     self.Navigation.Mgt.Stop = False
 
--- Software/Brain/Robot/Navigation.py
+++ Software/Brain/Robot/Navigation.py
@@ -1,6 +1,7 @@
-from threading import Thread
+from threading import Thread, Lock
+from IHM.interface import cst
 
-def Dijkstra(Start, End):
+def Dijkstra(End):
     pass #TBD
 
 class mgt():
@@ -24,6 +25,7 @@ class Navigation(Thread):
         Thread.__init__(self)
         self.path = 0 #TBD
         self.Mgt = mgt()
+        self.MUT = Lock()
 
     def Get_to_Point(self, point):
         Angle = Compute_Angle(point)
@@ -40,16 +42,23 @@ class Navigation(Thread):
     def Froward(self):
         pass #TBD     
 
+    def Wait_Start(self):
+        while self.Mgt.Stop:
+            self.Mgt.Waiting = True
+        self.Mgt.Waiting = False
+        return
+
     def run(self):
         self.Mgt.Stop = False
         self.Mgt.Waiting = False
         while True:
-            while self.Mgt.Stop:
-                self.Mgt.Waiting = True
-            self.Mgt.Waiting = False
+            self.Wait_Start()
             while not self.Mgt.Stop:
                 for i in self.path:
                     self.Get_to_Point(i)
                     if self.Mgt.Stop:
                         break
-                
+                    self.MUT.acquire()
+                    self.path = Dijkstra(cst.Home)
+                    self.MUT.release()
+                    
