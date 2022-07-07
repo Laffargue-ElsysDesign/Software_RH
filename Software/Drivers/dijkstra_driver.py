a054adf8436a6de1817451c5e139a6d6995c4fd8
Lenny Laffargue <lenny.laffargue@gmail.com>
2022-07-07 12:03:52 +0200

    Dijkstra macro and EKF Theta drift

--- Software/Drivers/dijkstra_driver.py
+++ Software/Drivers/dijkstra_driver.py
@@ -2,11 +2,16 @@ from pynq import DefaultIP, Overlay
 from signal import signal,SIGINT
 import time
 
+OFFSET_WRITE_REG = 0x00
+OFFSET_READ_STATUS = 0x04
+OFFSET_READ_REG = 0x08
+MASK_READ = 0x1F
+
+
 def handler(signal_received, frame):
     # Handle any cleanup here
     print('SIGINT or CTRL-C detected. HOLOCOM Exiting gracefully')
-    exit(0)               
-        
+    exit(0)                       
 
 class DijkstraDriver(DefaultIP):
     def __init(self, description):
@@ -16,24 +21,24 @@ class DijkstraDriver(DefaultIP):
     
     def Write_Data(self, StartPoint, StopPoint):
         data = (StopPoint << 16) + (StartPoint << 8) + 1
-        self.write(0x00, data)
+        self.write(OFFSET_WRITE_REG, data)
         
     def Read_Data(self, timeout = 1):
         stop_time = time.time() + timeout
-        while (not ((self.read(0x04) & 1) == 1)) and not (time.time()>stop_time):
+        while (not ((self.read(OFFSET_READ_STATUS) & 1) == 1)) and not (time.time()>stop_time):
             pass
         if time.time()>stop_time:
             raise ValueError("Read timeout")
-        nb_nodes = (self.read(0x08) & 0x1F)
+        nb_nodes = (self.read(OFFSET_READ_REG) & MASK_READ)
         data = []
         print(nb_nodes)
         for i in range(nb_nodes):
-            data.append((self.read((0x08)+((nb_nodes-i)*4)) & 0x1F))
+            data.append((self.read((OFFSET_READ_REG)+((nb_nodes-i)*4)) & MASK_READ))
         print(data)
         return data 
     
     def Disable(self):
-        self.write(0x00, 0)
+        self.write(OFFSET_WRITE_REG, 0)
         return 1
 
 class Dijkstra():
