77a81e61df324643746612add2edbeedf694d25f
Lenny Laffargue <lenny.laffargue@gmail.com>
2022-09-07 16:48:50 +0200

    Evitement V2

--- Software/Brain/Robot/Evitement.py
+++ Software/Brain/Robot/Evitement.py
@@ -37,6 +37,18 @@ class Evitement(Thread):
         self.speed_y = 0
         self.speed_z = 0
 
+        self.W = False
+        self.NW = False
+        self.N = False
+        self.NE = False
+        self.E = False
+
+        self.C_W = 0
+        self.C_NW = 0
+        self.C_N = 0
+        self.C_NE = 0
+        self.C_E = 0
+
         self.mgt = Mgt()
         self.interrupt = False
 
@@ -52,12 +64,65 @@ class Evitement(Thread):
             #print("Evitement Waiting..")
         self.mgt.Is_Not_Waiting()
         return 1
+
+    def Stop(self):
+        self.speed_x = 0
+        self.speed_y = 0
+        self.speed_z = 0
     
     def Get_Raw_Speed(self):
         (self.speed_x, self.speed_y, self.speed_z) = raw_command.Get()
 
     def Set_Command(self):
         cmd_robot.Set_Speed(self.speed_x, self.speed_y, self.speed_z)
+    
+    def Get_US(self):
+        
+        if not ultrasons.Get_W()[0]:
+            if not self.C_W == 0:
+                self.C_W = self.C_W - 1
+            else:
+                self.W = False
+        else:
+            self.C_W = 2 
+            self.W = True
+
+        if not ultrasons.Get_NW()[0]:
+            if not self.C_NW == 0:
+                self.C_NW = self.C_NW - 1
+            else:
+                self.NW = False
+        else:
+            self.C_NW = 2 
+            self.NW = True
+
+        if not ultrasons.Get_N()[0]:
+            if not self.C_N == 0:
+                self.C_N = self.C_N - 1
+            else:
+                self.N = False
+        else:
+            self.C_N = 2 
+            self.N = True
+
+        if not ultrasons.Get_NE()[0]:
+            if not self.C_NE == 0:
+                self.C_NE = self.C_NE - 1
+            else:
+                self.NE = False
+        else:
+            self.C_NE = 2 
+            self.NE = True
+
+        if not ultrasons.Get_E()[0]:
+            if not self.C_E == 0:
+                self.C_E = self.C_E - 1
+            else:
+                self.E = False
+        else:
+            self.C_E = 2 
+            self.E = True
+   
    
     def run(self):
         while not self.interrupt:
@@ -68,30 +133,24 @@ class Evitement(Thread):
                 
                 self.Get_Raw_Speed()
 
-                if ultrasons.Get_W()[0] or ultrasons.Get_NW()[0]:
+                self.Get_US()
+
+                if self.N or self.NW:
                     if self.speed_y < 0:
-                        self.speed_x = 0
-                        self.speed_y = 0
-                        self.speed_z = 0
+                        self.Stop()
 
-                if ultrasons.Get_NW()[0] or ultrasons.Get_N()[0] or ultrasons.Get_NE()[0]:
+                if self.NW or self.N or self.NE:
                     if self.speed_x > 0:
-                        self.speed_x = 0
-                        self.speed_y = 0
-                        self.speed_z = 0
+                        self.Stop()
 
-                if ultrasons.Get_E()[0] or ultrasons.Get_NE()[0]:
+                if self.E or self.NE:
                     if self.speed_y > 0:
-                        self.speed_x = 0
-                        self.speed_y = 0
-                        self.speed_z = 0
+                        self.Stop()
                     
 
                 self.Set_Command()
             
-            self.speed_x = 0
-            self.speed_y = 0
-            self.speed_z = 0
+            self.Stop()
             self.Set_Command()
 
 thread_evitement = Evitement()
